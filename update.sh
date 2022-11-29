e75f2bfee53c37f463c8cb0b8a5693cb00e34242
kowalp <kowalp@rpi.edu>
2022-11-28 23:03:02 -0500

    Reorganized TRANS rules

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 # Copy language server build
 #cp -r /home/peter/eclipse2022/MCNP-LS/gov.lanl.mcnp.parent/gov.lanl.mcnp.ide/build/install/gov.lanl.mcnp.ide/lib /home/peter/Research/mm_server_api/metapy
 
+cp -r /home/peter/eclipse2022/Serpent-LS/gov.lanl.serpent.parent/gov.lanl.serpent.ide/build/install/gov.lanl.serpent.ide/lib /home/peter/Research/mm_server_api/metapy
+
 # Recompile EntryPoint.jar
 cd /home/peter/Research/mm_server_api/metapy
 bash compile.sh
